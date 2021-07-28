94cf4b46f2228ba8cd369c7abeaed560ad7b5b09
Mach <43095947+Hoithmach@users.noreply.github.com>
2021-07-28 23:40:06 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# FGO Material Simulator Select All Servants
+Userscript that adds a "Select All" button to the [FGO Material Simulator](http://fgosimulator.webcrow.jp/Material/) servant selector under the Inventory (EN) / My Chaldea (JP) / 所持アイテム (JP) dialogue which automates clicking on every servant icon manually. Avoids having to individually select all 100+ servants on your account. Tested with Tampermonkey on Firefox.
+
+###### Known Issue~~s~~
+- The order the script selects in does not match any of the sort options within FGO. This is annoying and will **hopefully** be fixed *before* Gilfest 2.
+
+
+### Updates:
+#### v0.1
+Initial upload
+
+
+### See also:
+[FGO Material Simulator Multi Account](https://github.com/Hoithmach/fgo_material_simulator_multi_account) Userscript for managing multiple accounts
---
+++ fgo_material_simulator_select_all.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         FGO Material Simulator Select All Servants
+// @namespace    https://github.com/Hoithmach/fgo-material-simulator-select-all-servants
+// @version      0.1
+// @description  Adds a "Select All" button to the inventory dialogue
+// @author       Hoithmach
+// @match        http://fgosimulator.webcrow.jp/Material/
+// @icon         https://www.webcrow.jp/images/common/favicon.ico
+// ==/UserScript==
+
+
+function selectAll() {
+
+    document.getElementById("chaldea_select14").click();
+
+    var chaldeaSlide = document.getElementsByClassName("chaldea-slide")[0];
+
+    var servantButtons = chaldeaSlide.getElementsByClassName("input_chaldea");
+
+    const servantListLen = servantButtons.length;
+
+    for (var i = 0; i < servantListLen; i++) {
+
+        servantButtons[0].click();
+
+    };
+};
+
+
+(function() {
+
+    var servantSelectMenu = document.getElementById("chaldea_servant");
+    var classSelectBar = servantSelectMenu.getElementsByClassName("select-class")[0]
+
+    var selectAllButtonDiv = document.createElement("div");
+
+    selectAllButtonDiv.className = "selectAllButton";
+    selectAllButtonDiv.style.display = "flex";
+    selectAllButtonDiv.style.flexDirection = "row";
+    selectAllButtonDiv.style.alignItems = "center";
+    selectAllButtonDiv.style.flex = "initial";
+
+    var selectAllButton = document.createElement("input");
+
+    selectAllButton.id = "selectAllButton";
+    selectAllButton.type = "button";
+    selectAllButton.value = "Select All";
+    selectAllButton.style.display = "flex";
+
+    selectAllButton.onclick = selectAll;
+
+
+    selectAllButtonDiv.appendChild(selectAllButton);
+    classSelectBar.appendChild(selectAllButtonDiv);
+
+})();
